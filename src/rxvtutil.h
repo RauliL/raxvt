957ae4377971e89306b206fee70c55a2162ea125
Rauli Laine <rauli.laine@iki.fi>
2017-07-23 13:45:22 +0300

    Add utility function for extract XDG config directory

--- src/rxvtutil.h
+++ src/rxvtutil.h
@@ -1,8 +1,9 @@
 #ifndef RXVT_UTIL_H
 #define RXVT_UTIL_H
 
-#include <stdlib.h>
-#include <string.h>
+#include <cstdlib>
+#include <cstring>
+#include <string>
 #include "ecb.h"
 #include "estl.h"
 
@@ -69,24 +70,8 @@ struct stringvec : simplevec<char *>
   }
 };
 
-#if 0
-template<typename T>
-struct rxvt_vec : simplevec<void *>
-{
-  typedef T *iterator;
-
-  void push_back (T d) { simplevec<void *>::push_back ((void *)d); }
-  T pop_back () { return (T*)simplevec<void *>::pop_back (); }
-  void erase (int i) { erase (begin () + i); }
-  void erase (iterator i) { simplevec<void *>::erase ((void **)i); }
-  iterator begin () const { return (iterator)simplevec<void *>::begin (); }
-  iterator end () const { return (iterator)simplevec<void *>::end (); }
-  T &operator [] (int i) { return * (T *) (& ((* (simplevec<void *> *)this)[i])); }
-  const T &operator [] (int i) const { return * (const T *) (& ((* (const simplevec<void *> *)this)[i])); }
-};
-#endif
-
 typedef estl::scoped_array<char> auto_str;
 
-#endif
+std::string get_config_directory();
 
+#endif
